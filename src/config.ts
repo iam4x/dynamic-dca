0ce79441022da31a98106192bc0303baeba77baa
iam4x <iam4x@me.com>
2025-11-21 14:25:48 +0400

    feat(config): set TOTAL_CAPITAL as env

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 export const TOKEN = process.env.TOKEN || "BTCUSDT";
 
-export const TOTAL_CAPITAL = 12_500;
+export const TOTAL_CAPITAL = process.env.TOTAL_CAPITAL
+  ? Number(process.env.TOTAL_CAPITAL)
+  : 10_000;
+
 export const ALLOCATION_PERIOD = 30;
 export const BUY_INTERVAL_HOURS = 12;
 export const SENSITIVITY = 3.0;
