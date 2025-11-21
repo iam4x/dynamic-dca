30172bf87dfb86689f444953b12e7fb886acd25e
iam4x <iam4x@me.com>
2025-11-21 12:51:26 +0400

    feat(main): add logs after starting

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
 import { Cron } from "croner";
-import { redis } from "bun";
 
 import { executeBuy } from "./services/execute-buy";
 import { getCapitalMetrics } from "./services/track-performance";
 import { getState } from "./modules/state";
 import { logger } from "./modules/logger";
 
-try {
-  await redis.connect();
-} catch (error) {
-  logger.error({ msg: "Failed to connect to Redis", error });
-  process.exit(1);
-}
+logger.info("Starting Dynamic DCA...");
 
 // Register cron jobs
 // ------------------
 new Cron("0 */12 * * *", async () => await executeBuy());
 new Cron("0 * * * *", async () => await getCapitalMetrics());
 
+logger.info("Dynamic DCA started");
+
 // If no purchases have been made, execute a buy to start
 // ------------------------------------------------------
 if ((await getState()).PURCHASE_HISTORY.length === 0) {
+  logger.info("No purchases have been made, executing buy to start");
   await executeBuy();
+} else {
+  logger.info("Initial purchase have been made, skipping buy");
 }
